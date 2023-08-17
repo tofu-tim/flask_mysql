8d3e04402e91f2050814c2956bdb765e25893884
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-08-16 19:51:29 -0500

    Add files via upload

--- dojos.py
+++ dojos.py
@@ -1,6 +1,6 @@
-from flask import render_template, redirect, request
+from flask import Flask, render_template, redirect, request
 from flask_app import app
-from flask_app.models import Dojo, Ninja
+from flask_app.models import dojo, ninja
 
 @app.route('/dojos')
 def dojos():
