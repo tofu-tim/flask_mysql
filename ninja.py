bb7ca9ea58ab469f5a1db9b6c54b46e41a7f264e
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-08-22 20:51:12 -0500

    Add files via upload

--- ninja.py
+++ ninja.py
@@ -1,13 +1,16 @@
-from mysqlconnection import connectToMySQL
+from flask_app.config.mysqlconnection import connectToMySQL
+# from flask_app.models.dojo import Dojo
 
 class Ninja:
     def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
+        self.age = data['age']
         self.dojo_id = data['dojo_id']
         self.created_at = data['created_at']
-        self.updated_at = data['updated_at']    
+        self.updated_at = data['updated_at']   
+        self.whole_name = self.first_name + self.last_name 
 
     @classmethod
     def get_all(cls):
@@ -20,22 +23,22 @@ class Ninja:
 
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO ninjas (first_name,last_name,dojo_id) VALUES (%(first_name)s,%(last_name)s,%(dojo_id)s);"
+        query = "INSERT INTO ninjas (first_name,last_name,age,dojo_id) VALUES (%(first_name)s,%(last_name)s,%(age)s,%(dojo_id)s);"
         result = connectToMySQL('dojos_and_ninjas_schema').query_db(query,data)
         return result
 
     @classmethod
     def get_one(cls,data):
-        query  = "SELECT * FROM users WHERE id = %(id)s;"
+        query  = "SELECT * FROM ninjas WHERE id = %(id)s;"
         result = connectToMySQL('dojos_and_ninjas_schema').query_db(query,data)
         return cls(result[0])
 
     @classmethod
     def update(cls,data):
-        query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,dojo_id=%(dojo_id)s,updated_at=NOW() WHERE id = %(id)s;"
+        query = "UPDATE ninjas SET first_name=%(first_name)s,last_name=%(last_name)s,dojo_id=%(dojo_id)s,updated_at=NOW() WHERE id = %(id)s;"
         return connectToMySQL('dojos_and_ninjas_schema').query_db(query,data)
 
     @classmethod
     def destroy(cls,data):
-        query  = "DELETE FROM users WHERE id = %(id)s;"
+        query  = "DELETE FROM ninjas WHERE id = %(id)s;"
         return connectToMySQL('dojos_and_ninjas_schema').query_db(query,data)
