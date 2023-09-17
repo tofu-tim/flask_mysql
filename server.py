74acfcbe9bbfa21f4380f2e18c6db1299e3342e0
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-09-17 13:21:33 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -2,25 +2,55 @@ from flask import Flask, render_template, request, redirect
 
 from users import User
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     return redirect('/users')
 
+
 @app.route('/users')
 def users():
-    return render_template("read_all.html", users=User.get_all())
+    return render_template("read_all.html",users=User.get_all())
 
-@app.route('/users/new')
+
+@app.route('/user/new')
 def new():
     return render_template("create.html")
 
-@app.route("/users/create", methods=['POST'])
+@app.route('/user/create',methods=['POST'])
 def create():
+    print(request.form)
     User.save(request.form)
     return redirect('/users')
 
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("edit_user.html",user=User.get_one(data))
 
-if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port=8000)
+@app.route('/user/show/<int:id>')
+def show(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("show_user.html",user=User.get_one(data))
+
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data ={
+        'id': id
+    }
+    User.destroy(data)
+    return redirect('/users')
+
+if __name__=="__main__":
+    app.run(debug=True)
