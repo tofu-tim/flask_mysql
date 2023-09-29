5fcb0e3c026b94d6e9feb287b90eca0209e03cd3
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-09-28 19:51:57 -0500

    Add files via upload

--- ninjas.py
+++ ninjas.py
@@ -1,13 +1,53 @@
-from flask_app import app
 from flask import render_template, redirect, request
-from flask_app.models import dojo, ninja
+from flask_app import app
+from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
 
+@app.route('/')
+def index():
+    return render_template("dojos_main.html")
 
-@app.route('/ninjas')
+@app.route('/ninjas', methods=['GET', 'POST'])
 def ninjas():
-    return render_template('ninja.html', dojos = dojo.Dojo.get_all())
+    ninjas = Ninja.get_all()
+    return render_template('ninja.html', ninjas=ninjas)
 
-@app.route('/create/ninja',methods=['POST'])
+@app.route('/new/ninja')
+def new_ninja():
+    dojos = Dojo.get_all()
+    return render_template('new_ninja.html', dojos=dojos)
+
+@app.route('/create/ninja', methods=['POST'])
 def create_ninja():
-    ninja.Ninja.save(request.form)
+    Ninja.save(request.form)
     return redirect('/')
+
+@app.route('/ninjas/delete/<ninja_id>/<dojo_id>', methods=['POST'])
+def delete_ninja(ninja_id, dojo_id):
+    Ninja.destroy(request.form)
+    return redirect('/ninjas')
+
+
+# @app.route('/ninjas/edit/<id>', methods=['POST'])
+# def edit_page(id):
+#     ninja = Ninja.get_one(id)
+#     return render_template('edit_ninja.html', ninja=ninja)
+
+@app.route('/ninjas/edit/<id>', methods=['POST'])
+def edit_ninja(id):
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+
+    for ninja in ninjas:
+        if ninja['id'] == id:
+            ninja['first_name'] = first_name
+            ninja['last_name'] = last_name
+
+    return redirect('/ninjas')
+
+
+@app.route('/ninjas/update/<dojo_id>', methods=["POST"])
+def update_ninja(dojo_id):
+    Ninja.update(request.form)
+    print("Updating ninja information: ", request.form)
+    return redirect(f'/dojos/{dojo_id}')
