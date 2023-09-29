752e13d1a9a04f7a2b8d3e90568abffcfbdbb3d9
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-09-28 19:52:16 -0500

    Add files via upload

--- dojo.py
+++ dojo.py
@@ -1,44 +1,64 @@
-
 from flask_app.config.mysqlconnection import connectToMySQL
-from flask_app.models.ninja import Ninja
 
 class Dojo:
-    schema = 'dojo_and_ninjas_schema'
+    db = 'dojos_and_ninjas_schema'
     def __init__(self, data):
         self.id = data['id']
         self.name = data['name']
         self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
+        self.updated_at = data['updated_at']    
         self.ninjas = []
 
     @classmethod
+    def dojo_ninjas(cls, db, data):
+        from flask_app.models.ninja import Ninja
+
+    @classmethod
     def get_all(cls):
-        query = "SELECT * FROM dojos"
-        results = connectToMySQL(cls.schema).query_db(query)
+        query = "SELECT * FROM dojos;"
+        results = connectToMySQL(cls.db).query_db(query)
         dojos = []
-        for row in results:
-            dojos.append( cls(row) )
+        for dojo in results:
+            dojos.append( cls(dojo) )
         return dojos
 
     @classmethod
     def save(cls, data):
         query = "INSERT INTO dojos (name) VALUES (%(name)s);"
-        return connectToMySQL(cls.schema).query_db(query,data)
+        result = connectToMySQL(cls.db).query_db(query,data)
+        return result
 
     @classmethod
-    def get_one(cls, data):
-        query = "SELECT * FROM dojos JOIN ninjas ON dojos.id = ninjas.dojo_id WHERE dojos.id=%(id)s;"
-        results = connectToMySQL(cls.schema).query_db(query, data)
-        print(results)
+    def get_one(cls,data):
+        query  = "SELECT * FROM dojos WHERE id = %(id)s;"
+        result = connectToMySQL(cls.db).query_db(query,data)
+        return cls(result[0])
+
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE dojos SET name=%(name)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL(db).query_db(query,data)
+    
+    @classmethod
+    def dojo_ninjas(cls, db, data):
+        query = "SELECT * FROM dojos LEFT JOIN ninjas ON ninjas.dojo_id = dojos.id WHERE dojos.id = %(id)s;"
+        results = connectToMySQL(db).query_db(query, data)
+        if not results:
+            return None
+        
         dojo = cls(results[0])
-        for i in results:
-            row = {
-                'id': i['ninjas.id'],
-                'first_name': i['first_name'],
-                'last_name': i['last_name'],
-                'age': i['age'],
-                'created_at': i['ninjas.created_at'],
-                'updated_at': i['ninjas.updated_at'],
+        for row_from_db in results:
+            ninja_data = {
+                "id": row_from_db["ninja.id"],
+                "first_name": row_from_db["ninja.first_name"],
+                "last_name": row_from_db["ninja.last_name"],
+                "age": row_from_db["ninja.age"]
             }
-            dojo.ninjas.append( Ninja(row))
+            dojo.ninjas.append(Ninja(ninja_data))
         return dojo
+
+
+    @classmethod
+    def destroy(cls,data):
+        query  = "DELETE FROM dojos WHERE id = %(id)s;"
+        return connectToMySQL(db).query_db(query,data)
