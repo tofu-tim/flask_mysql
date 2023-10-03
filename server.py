fb09a39c5cb5fff7d6ece5c3c5a809407ba0013c
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-10-02 22:01:22 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,57 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask_app import app
 
-from users import User
-
-app=Flask(__name__)
-
-@app.route('/')
-def index():
-    return redirect('/users')
-
-
-@app.route('/users')
-def users():
-    return render_template("read_all.html",users=User.get_all())
-
-
-@app.route('/user/new')
-def new():
-    return render_template("create.html")
-
-@app.route('/user/create',methods=['POST'])
-def create():
-    print(request.form)
-    User.save(request.form)
-    return redirect('/users')
-
-@app.route('/user/edit/<int:id>')
-def edit(id):
-    data ={ 
-        "id":id
-    }
-    return render_template("edit_user.html",user=User.get_one(data))
-
-@app.route('/user/show/<int:id>')
-def show(id):
-    data ={ 
-        "id":id
-    }
-    return render_template("show_user.html",user=User.get_one(data))
-
-
-@app.route('/user/update', methods=['POST'])
-def update():
-    user_id = request.form["id"]
-    User.update(request.form)
-    return redirect(url_for('show', id=user_id))
-
-@app.route('/user/destroy/<int:id>')
-def destroy(id):
-    data ={
-        'id': id
-    }
-    User.destroy(data)
-    return redirect('/users')
+from flask_app.controllers import ninjas
+from flask_app.controllers import dojos
 
 if __name__=="__main__":
     app.run(debug=True)
